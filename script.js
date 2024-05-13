a452fbdb84c9737b433b5324eb076cdf3d32f090
Yash Koshta <162898129+yash-9589@users.noreply.github.com>
2024-05-13 15:07:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+//Js
+document.querySelector(".mobile-btn").addEventListener("click", function () {
+    document.querySelector(".menu").classList.toggle("active");
+  });
+
+  //https://raw.githubusercontent.com/programmercloud/pgc-gym/main/img/trainer3.jpg
